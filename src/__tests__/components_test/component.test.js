0472d0806868439f2c08206e08e5bb4ee5f825c8
IfzaRasool <ifzarasoolarain@gmail.com>
2022-05-20 15:32:14 +0300

    Update Read me file with deploy link -Add function testing

--- src/__tests__/components_test/component.test.js
+++ src/__tests__/components_test/component.test.js
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../../redux/configureStore';
 import Home from '../../components/Home';
+import DetailCountry from '../../components/DetailCountry';
 
 describe('test snapshots for all components', () => {
   const myHome = [];
+  const Detail = [];
   it('renders home page correctly', () => {
     const tree = renderer
       .create(
@@ -14,6 +16,19 @@ describe('test snapshots for all components', () => {
           <Home>
             {myHome.forEach(() => renderer.create(<Home />).toJSON())}
           </Home>
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders Detail page correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <DetailCountry>
+            {Detail.forEach(() => renderer.create(<DetailCountry />).toJSON())}
+          </DetailCountry>
         </Provider>,
       )
       .toJSON();
